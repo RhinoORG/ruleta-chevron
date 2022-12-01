3b3673dd9abe428c1c9be32e88396ddd7daa4fa0
carlosbult <acevedo.bult@gmail.com>
2022-12-01 15:49:59 -0400

    vista de ruleta lista

--- src/components/Ruleta.jsx
+++ src/components/Ruleta.jsx
@@ -3,9 +3,9 @@ import { usePlayers } from '../context/PlayersContext'
 
 const opts = [
   { name: 'QUIEBRA', value: 0, movements: 0 },
-  { name: 'QUIEBRA', value: 22.4, movements: 0 },
   { name: 'QUIEBRA', value: 110, movements: 0 },
   { name: 'PIERDE TURNO', value: 210, movements: 0 },
+  { name: 'COMODIN', value: 20, movements: 0 },
   { name: 'COMODIN', value: 240, movements: 0 },
 
   { name: 25, value: 330, movements: 1 },
@@ -24,25 +24,26 @@ const opts = [
   { name: 100, value: 130, movements: 4 },
 ]
 
-function getRandomIdx (min, max) {
+function getRandomIdx(min, max) {
   return Math.floor(Math.random() * (max - min) + min)
 }
 
-function Ruleta ({ isVisible, playerActive, removeRuletaFromRuleta }) {
+function Ruleta({ isVisible, playerActive, removeRuletaFromRuleta }) {
   const { players, setPlayerTurn, playerTurn } = usePlayers()
   const [rotateDeg, setRotateDeg] = useState(0)
   const [reward, setReward] = useState('')
-  const [movements, setMovements] = useState(0)
-  const [finish, setFinish] = useState(false)
+  const [movements, setMovements] = useState('')
+  const [finish, setFinish] = useState(true)
   const [showButton, setShowButton] = useState(false)
+  const [showMessage, setShowMessage] = useState(false)
 
   if (!isVisible) return null
 
-  console.log()
-
-  function rotate () {
+  function rotate() {
     const idx = getRandomIdx(1, 10)
     const opt = opts[idx]
+
+    console.log(opt)
 
     setRotateDeg(opt.value)
 
@@ -56,26 +57,37 @@ function Ruleta ({ isVisible, playerActive, removeRuletaFromRuleta }) {
       if (reward == 'COMODIN') {
         player.puntos = 500 + player.puntos;
         setShowButton(false)
-        setFinish(false)
-        setMovements('Puedes girar la ruleta de nuevo')
+        setFinish(true)
+        setShowMessage(true)
+        setMovements('Has ganado 500 pts. Puedes girar la ruleta de nuevo')
         return
       }
 
       if (reward == 'QUIEBRA') {
         player.puntos = player.puntos - player.puntos;
-        setShowButton(true)
+        setFinish(true)
+        setShowMessage(true)
         setMovements('Te has quedado sin puntos')
-        return
+
+        const playersCantity = players.length
+
+        if (playerTurn < playersCantity - 1) {
+          setPlayerTurn(playerTurn + 1)
+        }
+        if (playerTurn === playersCantity - 1) {
+          setPlayerTurn(0)
+        }
       }
 
       if (reward == 'PIERDE TURNO') {
         setMovements('Haz perdido el turno')
-        setFinish(false)
+        setFinish(true)
+        setShowMessage(true)
         setShowButton(false)
         player.puntos = player.puntos
-        
+
         const playersCantity = players.length
-        
+
         if (playerTurn < playersCantity - 1) {
           setPlayerTurn(playerTurn + 1)
         }
@@ -85,37 +97,50 @@ function Ruleta ({ isVisible, playerActive, removeRuletaFromRuleta }) {
 
       }
 
-      setShowButton(true)
-      setMovements(`Haz ganado ${opt.movements} letras`)
+      if (typeof reward !== 'string') {
+        setShowMessage(true)
+        setShowButton(true)
+        setFinish(false)
+        setMovements(`Haz ganado ${opt.movements} letras`)
 
-      if (typeof reward !== 'string') player.puntos = player.puntos + reward
-    }, 5000)
+        player.puntos = player.puntos + reward
+      }
 
-    setFinish(false)
+      window.localStorage.setItem('players', `${JSON.stringify(players)}`)
+    }, 6000)
+
+    setFinish(true)
   }
 
   return (
     <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm  flex justify-center items-center flex-col'>
-      
-      {!finish && (
-      <>
-      <div className='w-3 h-14 bg-cyan-500 rounded-b-xl relative -bottom-6 z-50' />
-      
-      <img src='/ruleta.png' style={{ transition: '5s all', transform: `rotate(${rotateDeg}deg` }} className='w-[400px] h-[400px] rounded-full' onClick={rotate} alt='' />
-      </>)}
-
 
       {finish && (
         <>
-          <h2 className='text-gray-200 text-5xl my-7 uppercase font-medium'>{reward}</h2>
-          <h4 className='text-gray-200 4text-3xl my-5 uppercase font-medium'>{movements}</h4>
-          {showButton &&
-          <button className='bg-lime-500 px-5 py-2 my-4 rounded-md font-semibold relative text-white' onClick={() => { 
-            setFinish(false);
-            removeRuletaFromRuleta()
-            }}>Jugar</button>}
-        </>
-      )}
+          <div className='w-3 h-14 bg-cyan-500 rounded-b-xl relative -bottom-6   z-50' />
+
+          <img src='/ruleta.png' style={{ transition: '5s all', transform: `rotate(${rotateDeg}deg` }} className='w-[400px] h-[400px]   rounded-full' onClick={rotate} alt='' />
+        </>)
+      }
+
+      {showMessage && <>
+        <h2 className='text-gray-200 text-5xl my-7 uppercase font-medium'>{reward}</h2>
+        <h4 className='text-gray-200 4text-3xl my-5 uppercase font-medium'>{movements}</h4>
+      </>
+      }
+
+      {showButton &&
+        <button
+          className='bg-lime-500 px-5 py-2 my-4 rounded-mfont-semibold  relative text-white'
+          onClick={() => {
+            setFinish(true);
+            removeRuletaFromRuleta(false)
+            setShowMessage(false)
+            setShowButton(false)
+          }}>
+          Jugar
+        </button>
+      }
     </div>
   )
 }
