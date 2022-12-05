4bc4b1cb50f3b8b7586c1f75011e88271373ba18
Jhonathan <Jhonathanjhs10@gmail.com>
2022-12-05 16:53:33 -0400

    Adding styles and revelar action in regular game

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -425,7 +425,7 @@ function Game() {
         </div>
       )}
       <button
-        className="absolute w-14 h-14 rounded-full right-3 top-2 border-4 border-gray-800 text-white text-2xl uppercase font-black transition hover:scale-105 btn-shadow"
+        className="absolute w-14 h-14 rounded-full right-3 top-2 border-4 bg-gray-800 border-gray-800 text-white text-2xl uppercase font-black transition hover:scale-105 btn-shadow"
         onClick={() => setFinishGameModal((prev) => !prev)}
       >
         X
